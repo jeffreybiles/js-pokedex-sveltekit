2b834f9b789e591a66de81d767be4ed089b88157
Jeffrey Biles <bilesjeffrey@gmail.com>
2023-01-02 11:07:32 -0600

    lesson 02 - getting started

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+  import { hello } from "./hello";
+</script>
+
+<h1>Welcome to {hello}!!!!</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+
+</style>
