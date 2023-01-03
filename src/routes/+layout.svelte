ce8f706c0c565c5273db7175139cf4ce7f5efc2e
Jeffrey Biles <bilesjeffrey@gmail.com>
2023-01-03 12:32:58 -0600

    Link back to tutorials

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
   import { page } from '$app/stores';
+  import { productionApi } from '$lib/production-api';
+  import type { LayoutData } from './$types';
+  import type { Lesson } from '$lib/types';
+  
+  export let data: LayoutData;
+
+  $: lesson = data.lesson as Lesson
 </script>
 
 <div class="navbar">
@@ -10,6 +17,18 @@
 <div class="container">
   <slot />
 </div>
+
+{#if lesson}
+  <!-- svelte-ignore security-anchor-rel-noreferrer -->
+  <a
+    class="lesson-modal-button"
+    href="https://{productionApi}/lessons/{lesson.id}"
+    target="_blank"
+  >
+    This is the state of the app for "Lesson {lesson.number} - {lesson.name}".<br>
+    Click here for source code and tutorials for making this site in {lesson.implementations.map(implementation => implementation.framework).join(', ')}.
+  </a>
+{/if}
 
 <style>
   .navbar {
@@ -43,5 +62,21 @@
     max-width: 90%;
     margin: 0 auto;
     width: 1200px;
+  }
+
+  .lesson-modal-button {
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    margin: 16px;
+    padding: 8px 16px;
+    font-size: 16px;
+    background-color: #333333;
+    color: #FFFFFF;
+    cursor: pointer;
+    border-radius: 8px;
+    box-shadow: 0 0 40px 10px rgba(0, 0, 0, 0.7);
+    border: none;
+    text-decoration: none;
   }
 </style>
