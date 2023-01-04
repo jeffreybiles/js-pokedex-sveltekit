d5d7ee66ee8aee81ef4e27b439d070257d47bb05
Jeffrey Biles <bilesjeffrey@gmail.com>
2023-01-04 16:57:19 -0600

    06 - reactivity

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
 import type { PageData } from "./$types";
+import type { IndexMonster } from "./+page";
 import { generations } from "./generations";
 
 export let data: PageData;
+let monsterId: string;
+$: monster = data.monsters.find((monster) => monster.id === monsterId);
+
+const monsterClick = (monster: IndexMonster) => {
+  monsterId = monster.id;
+};
 </script>
 
+<h1>{monsterId}</h1>
+<h2>{monster?.name}</h2>
 <div class="generations">
 {#each generations as generation (generation.id)}
   <div class="generation">{generation.main_region}</div>
@@ -14,7 +23,8 @@ export let data: PageData;
 <div class="monsters">
   {#each data.monsters as monster (monster.id)}
     
-    <div class="monster">
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <div class="monster" on:click={() => monsterClick(monster)}>
       <div class="monster-content">
         <img src={monster.image} alt={monster.name} />
         {monster.name}
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,13 +1,17 @@
 import type { PageLoad } from "./$types"
 
-type IndexMonster = {
+type ApiMonster = {
   name: string
   url: string
+}
+export type IndexMonster = ApiMonster & {
+  id: string
+  image: string
 }
 export const load = (async ({ fetch }) => {
   const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
   const json = await response.json()
-  const monsters = json.results.map((monster: IndexMonster) => {
+  const monsters: IndexMonster[] = json.results.map((monster: ApiMonster) => {
     const splitUrl = monster.url.split('/')
     const id = splitUrl[splitUrl.length - 2]
     return {
